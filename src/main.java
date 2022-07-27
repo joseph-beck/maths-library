09ae2249021a08adbc46d89430bea1154b425aea
plsnotjoe <96671050+plsnotjoe@users.noreply.github.com>
2022-07-27 22:12:12 +0100

    v1.0

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ import math.*;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println(degrees.cot(45));
+        System.out.println("Hello world!");
     }
 }
