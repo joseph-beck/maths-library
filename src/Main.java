a099e678bc811ebbd844653801fa51b6463be4bf
plsnotjoe <96671050+plsnotjoe@users.noreply.github.com>
2022-08-24 14:18:30 +0100

    v0.01

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Window.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Window window = new Window();
+        Window window = new Window();
 
         Matrix4d matrix1 = new Matrix4d(1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8);
         Matrix4d matrix2 = new Matrix4d(1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8);
